8d527a6eee52285ff052d8bccd98c8c004a87e88
itekin156 <mzkin.ramo@gmail.com>
2022-05-26 00:21:21 +0300

    create model

---
+++ app/src/main/java/com/example/pren/Model/student.java
@@ -0,0 +1,29 @@
+package com.example.pren.Model;
+
+public class student
+{
+    private int id;
+    private String nameStudent;
+    private int grade;
+    private String nikeName;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNameStudent() {
+        return nameStudent;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public String getNikeName() {
+        return nikeName;
+    }
+}
